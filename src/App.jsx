5f0e2312b9ccb2cc9237a2c1e22d97bce1f88f35
anshu1698 <61243671+anshu1698@users.noreply.github.com>
2022-08-29 16:37:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Dashboard} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
